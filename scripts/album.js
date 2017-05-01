da8b2b1886d2c40a72eb095c110fb84a94ff632e
naterchrds <me@nathandarrell.com>
2017-05-01 13:08:50 -0700

    Assignment 13 complete

--- scripts/album.js
+++ scripts/album.js
@@ -61,7 +61,12 @@ var setCurrentAlbum = function(album) {
 };
 
 var findParentByClassName = function(element, targetClass) {
-    if (element) {
+    if (this.parentElement === null) {    
+        console.log('No parent found');
+    } 
+    if (targetClass.className === null) {
+        consol.log('No parent found with that class name');
+    } else if (element) {
         var currentParent = element.parentElement;
         while (currentParent.className !== targetClass && currentParent.className !== null) {
             currentParent = currentParent.parentElement;
